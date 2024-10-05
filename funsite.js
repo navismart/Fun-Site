be25df2ac5c695ca70f2a901c673d19e4037566b
navihex <navicheban@gmail.com>
2024-10-05 18:19:21 -0400

    commi

--- funsite.js
+++ funsite.js
@@ -49,7 +49,8 @@ function invisibleCursor() {
 }
 
 function friedChickenCursor() {
-  document.body.style.cursor = "url(/chicken-cursor.cur), auto";
+  document.body.style.cursor =
+    "url(https://github.com/navismart/Fun-Site/blob/main/chicken-cursor.cur), auto";
 }
 
 function changeTitle() {
